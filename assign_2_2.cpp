3cdcda246f924f0ba0272f5e356758f1e696f3de
Faisal M S <me23b105@smail.iitm.ac.in>
2026-02-15 20:32:19 +0530

    assign_2_2.cpp

--- assign_2_2.cpp
+++ assign_2_2.cpp
@@ -1,3 +1,24 @@
+/* 
+(30 points) A large industrial furnace, as shown in the figure below, is supported on a long column of fireclay brick.
+During steady-state operation, installation is such that three surfaces of the column are maintained at 500 K while
+the remaining surface is exposed to an airstream at a temperature of 300 K. Owing to the symmetry of the problem
+the number of unknowns (temperature at the interior nodal locations) is reduced from 12 to 8. Using energy balance
+the following equations are obtained in terms of the nodal temperatures:
+
+Node 1: T2 + T3 + 1000 − 4T1 = 0
+Node 3: T1 + T4 + T5 + 500 − 4T3 = 0
+Node 5: T3 + T6 + T7 + 500 − 4T5 = 0
+Node 2: 2T1 + T4 + 500 − 4T2 = 0
+Node 4: T2 + 2T3 + T6 − 4T4 = 0
+Node 6: T4 + 2T5 + T8 − 4T6 = 0
+Node 7: 2T5 + T8 + 2000 − 9T7 = 0
+Node 8: 2T6 + 2T7 + 1500 − 9T8 = 0.
+
+Using the Gaussian elimination algorithm developed above, calculate and report the temperatures T1 through T8.
+Also, plot the variation of temperature as a function of y at locations x = 0.25 and x = 0.5.
+*/
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -51,4 +72,5 @@ T_5 = 436.95
 T_6 = 418.739
 T_7 = 356.995
 T_8 = 339.052
+
 */
